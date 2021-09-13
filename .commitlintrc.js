d4b5bb5f8374bfd4be6c3dac0b6334cb2cc900fd
EveCoffee <wangchaoyi.tk@gmail.com>
2021-09-13 16:15:21 +0800

    build(vuepress): add docs scope

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,10 +2,15 @@ const fs = require('fs')
 const path = require('path')
 
 const packages = fs.readdirSync(path.resolve(__dirname, 'packages/@vuepress'))
+const docsPackages = fs.readdirSync(path.resolve(__dirname, 'docs/zh'))
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['vuepress', 'vuepress-vite', ...packages]],
+    'scope-enum': [
+      2,
+      'always',
+      ['vuepress', 'vuepress-vite', ...packages, ...docsPackages],
+    ],
   },
 }
