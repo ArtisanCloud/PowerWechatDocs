e0def8b819dbcef1c765d07397d4d211eea9fd1b
EveCoffee <wangchaoyi.tk@gmail.com>
2021-10-26 13:38:08 +0800

    refactor(theme-default): update home style and interaction behavior

--- docs/.vuepress/configs/navbar/zh.ts
+++ docs/.vuepress/configs/navbar/zh.ts
@@ -3,7 +3,7 @@ import type { NavbarConfig } from '@vuepress/theme-default'
 export const zh: NavbarConfig = [
   {
     text: '首页',
-    link: '/zh/',
+    link: '/',
   },
   {
     text: `文档`,
@@ -13,16 +13,16 @@ export const zh: NavbarConfig = [
         link: '/zh/start/',
       },
       {
-        text: '企业微信',
-        link: '/zh/wecom/',
-      },
-      {
         text: '小程序',
         link: '/zh/mini-program/',
       },
       {
         text: '微信支付',
         link: '/zh/payment/',
+      },
+      {
+        text: '企业微信',
+        link: '/zh/wecom/',
       },
     ],
   },
